21678a49e826db249f04d1c2b69b3c6d5b8692e9
Ananda <anewton35@lancers.lenoircc.edu>
2026-08-27 11:04:17 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -11,9 +11,40 @@ let skills = ["JavaScript", "HTML", "CSS", "GitHub"];
 let skillsSection = document.querySelector("#Skills");
 let skillsList = skillsSection.querySelector("ul");
 
-for (let i = 0; i < skills.length; i++) {
+for (let i = 0; i <skills.length; i++) {
     let skill = document.createElement("li");
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
 
+let messageForm = document.forms["leave_message"];
+messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let name = event.target.userName.value;
+    let email = event.target.usersEmail.value;
+    let message = event.target.userMessage.value;
+
+    console.log(name, email, message);
+    messageForm.reset();
+});
+
+let messageSection = document.querySelector("#messages");
+let messageList = messageSection.querySelector("ul");
+
+let newMessage = document.createElement("li");
+newMessage.innerHTML = 
+    `<a href="mailto: ${email}">${name}</skills.length;>
+    <span>${message}</span>`;
+messageList.appendChild(newMessage);
+
+let removeButton = document.createElement("button");
+removeButton.textContent = "remove";
+removeButton.type = "button";
+
+removeButton.addEventListener("click", function () {
+    let entry = removeButton.parentNode;
+    entry.removeChild();
+});
+
+newMessage.appendChild(removeButton);
